8ad4a6f825e3fbfbc103d71af6b8b868475455bd
armaan535kh <kh.armaan535@gmail.com>
2024-08-07 19:37:01 -0700

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Armaan Khan  on 07/08/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
